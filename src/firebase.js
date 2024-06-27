8b04bdda9659707821e8f7e1b754235568132151
pchko <olga.pachowka@gmail.com>
2024-06-27 16:39:06 +0200

    change firebase variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 const firebaseConfig = {
-  apiKey: "AIzaSyD1uWSzs4GwaZhf49JFfx1T2T7vvvZn43g",
-  authDomain: "front-news-end.firebaseapp.com",
-  projectId: "front-news-end",
-  storageBucket: "front-news-end.appspot.com",
-  messagingSenderId: "361688447081",
-  appId: "1:361688447081:web:9306c5cb0576da28144af8",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 const app = initializeApp(firebaseConfig);

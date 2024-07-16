a3882be59dc63c52f1c48ac3d148ce50cc34ff2e
pchko <olga.pachowka@gmail.com>
2024-07-16 18:58:08 +0200

    wishlist styling

--- src/layouts/Wishlist/Wishlist.jsx
+++ src/layouts/Wishlist/Wishlist.jsx
@@ -30,59 +30,61 @@ const Wishlist = () => {
 
   return (
     <div className={`wishlist`} data-theme={theme}>
-      <div className="wishlist__headline">
-        <h2 className="wishlist-hdl">
-          {currentUser
-            ? `${currentUser.email}'s Read later list`
-            : "Read later list"}
-        </h2>
-      </div>
-
-      {wishlist.length === 0 ? (
-        <div className="wishlist__body">
-          <p className="">No articles in wishlist</p>
+      <div className="wishlist__main">
+        <div className="wishlist__headline">
+          <h3 className="wishlist-hdl">
+            {currentUser
+              ? `${currentUser.email}'s read later list`
+              : "Read later list"}
+          </h3>
         </div>
-      ) : (
-        // <div className="">
-        //   {wishlist.map((article, index) => (
-        //     <NewsElemet
-        //       key={index}
-        //       title={article.title}
-        //       url={article.url}
-        //       urlToImage={article.urlToImage}
-        //       source={article.source.name}
-        //       publishedAt={article.publishedAt}
-        //       onRemove={handleRemoveArticle}
-        //     />
-        //   ))}
-        // </div>
-        <ul>
-          {wishlist.map((article, index) => (
-            <div className="wishlist__article" key={index}>
-              <div className="wishlist__img">
-                <img
-                  className="article-img"
-                  src={article.urlToImage}
-                  alt={article.urlToImage}
-                />
+
+        {wishlist.length === 0 ? (
+          <div className="wishlist__body">
+            <p className="">No articles in wishlist</p>
+          </div>
+        ) : (
+          // <div className="">
+          //   {wishlist.map((article, index) => (
+          //     <NewsElemet
+          //       key={index}
+          //       title={article.title}
+          //       url={article.url}
+          //       urlToImage={article.urlToImage}
+          //       source={article.source.name}
+          //       publishedAt={article.publishedAt}
+          //       onRemove={handleRemoveArticle}
+          //     />
+          //   ))}
+          // </div>
+          <ul className="wishlist__list">
+            {wishlist.map((article, index) => (
+              <div className="wishlist__article" key={index}>
+                <div className="wishlist__img">
+                  <img
+                    className="article-img"
+                    src={article.urlToImage}
+                    alt={article.urlToImage}
+                  />
+                </div>
+                <div className="wishlist__title">
+                  <h4 className="article-title">
+                    <a href={article.url}>{article.title}</a>
+                  </h4>
+                </div>
+                <button
+                  className="wishlist__btn"
+                  onClick={() => handleRemoveArticle(article.url)}
+                >
+                  <Tooltip content={"Delete"}>
+                    <MdDeleteOutline />
+                  </Tooltip>
+                </button>
               </div>
-              <div className="wishlist__article--title">
-                <h4>
-                  <a href={article.url}>{article.title}</a>
-                </h4>
-              </div>
-              <button
-                className=""
-                onClick={() => handleRemoveArticle(article.url)}
-              >
-                <Tooltip content={"Delete"}>
-                  <MdDeleteOutline />
-                </Tooltip>
-              </button>
-            </div>
-          ))}
-        </ul>
-      )}
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 };
